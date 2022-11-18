2efd2793be507b2ff060d39ac63d7801f0a20900
OKURA Masafumi <masafumi.o1988@gmail.com>
2022-11-18 10:33:29 +0900

    Feat: Add Base64

--- src/index.tsx
+++ src/index.tsx
@@ -2,6 +2,7 @@ import { ActionPanel, Detail, List, Action } from "@raycast/api";
 
 import UNIXTimeConverter from "./unix-time-converter";
 import JSONBeautify from "./json-beautify";
+import Base64 from "./base64";
 
 export default function Command() {
   return (
@@ -30,6 +31,15 @@ export default function Command() {
         actions={
           <ActionPanel>
             <Action.Push title="Use this!" target={<JSONBeautify />} />
+          </ActionPanel>
+        }
+      />
+      <List.Item
+        icon="list-icon.png"
+        title="Base64"
+        actions={
+          <ActionPanel>
+            <Action.Push title="Use this!" target={<Base64 />} />
           </ActionPanel>
         }
       />
